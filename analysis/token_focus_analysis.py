7bea4c77f0c3b545738866a289ed811c2d09cb4b
Javaher <javahernourian@gmail.com>
2026-02-20 16:13:25 +0330

    initialize token focus analysis script

---
+++ analysis/token_focus_analysis.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+import config
+
+from load_hidden_states import load_hidden_states
