bdd62cb16525de22b1c860a870ddd961c459d0bd
Javaher <javahernourian@gmail.com>
2026-02-20 11:07:43 +0330

    initialize config to provide global setting

---
+++ config.py
@@ -0,0 +1,27 @@
+# ---------------------------
+# Global Experiment Settings
+# ---------------------------
+
+DATA_PATH = "data/hidden_states/gpt2_hidden_states.pt"
+
+PROMPT = "The capital of France is"
+TARGET_TOKEN = "ĠFrance"
+
+# ---------------------------
+# Plot Settings
+# ---------------------------
+
+FIG_SIZE_SMALL = (6, 4)
+FIG_SIZE_LARGE = (8, 6)
+
+COLOR_PRIMARY = "#1f4fa3"     # deep blue
+COLOR_SECONDARY = "#444444"   # dark grey
+COLOR_ACCENT = "#000000"      # black
+
+LINE_WIDTH = 2.0
+MARKER_SIZE = 6
+
+FONT_SIZE_TITLE = 12
+FONT_SIZE_LABEL = 10
+
+DPI = 120
